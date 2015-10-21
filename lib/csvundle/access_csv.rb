184f9b4775c56b53a6604486b7be00055b0d4e09
Jonah Ruiz <jonah@pixelhipsters.com>
2015-10-20 21:03:14 -0400

    Typecast key to symbol in CSVundle::GenericCSV#rows_for
    Additionally:
      - Remove instance variables (already have attr_accessor), so
        we can call them like regular methods
      - Refactor select case into registry + proc pattern
        (could be metaprogrammed)

--- lib/csvundle/access_csv.rb
+++ lib/csvundle/access_csv.rb
@@ -20,22 +20,26 @@ module CSVundle
     end
 
     def normalized_columns
-      @columns.map { |c| viperize(c) }
+      columns.map { |c| viperize(c) }
     end
 
     def rows_for(column)
-      { "#{column}": @rows.map { |row| row[@columns.index(column)] } }
+      { "#{column}".to_sym => rows.map { |row| row[columns.index(column)] } }
     end
   end
 
   class AccessCSV < GenericCSV
+    CSV_FORMATS = {
+      liencloud: -> {
+        YAML.load(File.read("config/headers.yml"))['liencloud']
+      },
+      lumentum: -> {
+        YAML.load(File.read("config/headers.yml"))['lumentum']
+      }
+    }.freeze
+
     def initialize(type)
-      case type
-      when :liencloud
-        @columns = YAML.load(File.read("config/headers.yml"))['liencloud']
-      when :lumentum
-        @columns = YAML.load(File.read("config/headers.yml"))['lumentum']
-      end
+      @columns = CSV_FORMATS[type].call
     end
   end
 end
