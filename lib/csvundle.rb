e960f7579703dd14e68d5bba8efa184057626a79
Ybur Yug <bobbygrayson@gmail.com>
2015-10-23 12:20:37 -0400

    symbolize keys

--- lib/csvundle.rb
+++ lib/csvundle.rb
@@ -3,6 +3,7 @@ require 'csvundle/access_csv'
 
 module CSVundle
   def self.setup_data_for(type)
-    CSVundle::AccessCSV.new(type.to_sym).setup_data
+    setup_data = CSVundle::AccessCSV.new(type.to_sym).setup_data
+    Hash[setup_data.map{|(k,v)| [k.to_sym,v]}] # convert string keys to symbols
   end
 end
